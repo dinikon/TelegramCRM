2c2a889f7dfac9ed06e33c92aa19893023a99f31
inikon <denis.inikon@gmail.com>
2024-10-12 11:40:06 +0300

    set bot name to widget

--- vue-project/src/views/authentication/SignIn.vue
+++ vue-project/src/views/authentication/SignIn.vue
@@ -35,7 +35,7 @@ onMounted(() => {
   const script = document.createElement('script');
   script.async = true;
   script.src = 'https://telegram.org/js/telegram-widget.js?22';
-  script.setAttribute('data-telegram-login', 'FinRockAppDev_bot');
+  script.setAttribute('data-telegram-login', 'inikon_dev_bot');
   script.setAttribute('data-size', 'large');
   script.setAttribute('data-onauth', 'onTelegramAuth(user)');
   script.setAttribute('data-request-access', 'write');
